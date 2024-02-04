3a0c0e203e2b89be75549faa7a00cf33d3ce187c
kanwar Dhillon <kdhillon469@gmail.com>
2024-02-04 13:53:36 +1100

    middleware and verifyemail page added

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+
+export default function VerifyEmailPage() {
+
+    const [token, setToken] = useState("");
+    const [verified, setVerified] = useState(false);
+    const [error, setError] = useState(false);
+
+    const verifyUserEmail = async () => {
+        try {
+          const response=  await axios.post('/api/users/verifyemail', {token})
+           const {status}  =await response
+           console.log('status is ',status)
+            setVerified(true);
+        } catch (error:any) {
+            setError(error.message);
+            console.log(error.message);
+            
+        }
+
+    }
+
+    useEffect(() => {
+        const urlToken = window.location.search.split("=")[1];
+        setToken(urlToken || "");
+    }, []);
+
+
+    useEffect(() => {
+        if(token.length > 0) {
+            verifyUserEmail();
+        }
+    }, [token]);
+
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+
+            <h1 className="text-4xl">Verify Email</h1>
+            <h2 className="p-2 bg-orange-500 text-black">{token ? `${token}` : "no token"}</h2>
+
+            {verified && (
+                <div>
+                    <h2 className="text-2xl">Email Verified</h2>
+                    <Link href="/login">
+                        Login
+                    </Link>
+                </div>
+            )}
+            {error && (
+                <div>
+                    <h2 className="text-2xl bg-red-500 text-black">Error:{error}</h2>
+                    
+                </div>
+            )}
+        </div>
+    )
+
+}
