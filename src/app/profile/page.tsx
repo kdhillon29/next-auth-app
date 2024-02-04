b6a74a0e83e427d2139cd76334f9c61c93d7b153
kanwar Dhillon <kdhillon469@gmail.com>
2024-02-04 20:43:11 +1100

    fixed typescript error

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,51 +1,67 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
-import React, {useState} from "react";
-import {Toaster, toast} from "react-hot-toast";
-import {useRouter} from "next/navigation";
-
+import React, { useEffect, useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
-    const router = useRouter()
-    const [user, setUser] = useState({})
-    const logout = async () => {
-        try {
-            await axios.get('/api/users/logout')
-            toast.success('Logout successful')
-            router.push('/login')
-        } catch (error:any) {
-            console.log(error.message);
-            toast.error(error.message)
-        }
+  const router = useRouter();
+  type IUser = {
+    [key: string]: any;
+  };
+  const [user, setUser] = useState<IUser>({});
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
     }
+  };
 
-    const getUserDetails = async () => {
-        const res = await axios.get('/api/users/profile')
-        console.log(res.data.user);
-        setUser(res.data.user)
-    }
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/profile");
+    console.log(res.data.user);
+    setUser(res.data.user);
+  };
+  useEffect(() => {
+    getUserDetails();
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <Toaster/>
-            <h1>Profile</h1>
-            <hr />
-            <p>Profile page</p>
-            <h2 className="p-1 rounded bg-green-500">{!user ? "Nothing" : <Link href={`/profile/${user?.username}`}>{user?.username}
-            </Link>}</h2>
-        <hr />
-        <button
+    return () => {
+      // second
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Toaster />
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {!user ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${user?.username}`}>{user?.username}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
         onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >Logout</button>
+      >
+        Logout
+      </button>
 
-        <button
+      <button
         onClick={getUserDetails}
         className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >GetUser Details</button>
-
-
-            </div>
-    )
+      >
+        GetUser Details
+      </button>
+    </div>
+  );
 }
